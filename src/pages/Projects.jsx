56e75060b0416030f255a0c79e6b8cb8f6278f76
MD. NAEEM UDDIN <mdnaeemuddin14@gmail.com>
2025-06-17 22:10:39 +0600

    Update Projects.jsx
    Change to project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Projects = () => {
   return (
     <div className="h-screen bg-[linear-gradient(45deg,_#15355e,_#0e1728,_#15355e,_#0e1728)] lg:px-10">
-      <h1 className="text-white text-3xl">Projects</h1>
+      <h1 className="text-white text-3xl">This is Projects Page</h1>
       
     </div>
   )
